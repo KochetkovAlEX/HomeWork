25b95d20e6feee325d2183cd6b6a38b561d73129
KochetkovAlEX <127874298+KochetkovAlEX@users.noreply.github.com>
2023-04-21 21:19:02 +0300

    Update script.js

--- vs_code/js/script.js
+++ vs_code/js/script.js
@@ -84,18 +84,3 @@
 // min=+prompt("Введите минуты")
 // sec=+prompt("Введите секунды")
 // console.log(seconds(h,min,sec))
-
-
-const array=[1,2,3,4], array2=[1,2,7]
-    function uniq(array,array2){
-    let array3=[]
-    for (let i=0;i<array2.length;i++){
-        if (!(array2[i] in array)) array3.push(array2[i])
-    }
-    for (let i=0;i<array.length;i++){
-        if (!(array[i] in array2)) array3.push(array[i])
-    }
-    array3.sort()
-    return array3
-}
-console.log(uniq(array,array2))
